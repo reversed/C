9ed2e24784602389b631056536797018366b4279
reversed <leichen.usst@gmail.com>
2016-10-17 13:33:07 +0800

    add stack dump

--- LuaTest.c
+++ LuaTest.c
@@ -20,3 +20,31 @@ int main(void) {
     lua_close(L);
     return 0;
 }
+
+static void stackDump (lua_State *L) {
+    int i;
+    int top = lua_gettop(L);                // depth of the stack
+    for (i = 1; i <= top; ++i) {
+        int t = lua_type(L, i);
+        switch (t) {
+            case LUA_TSTRING: {
+                printf("'%s'", lua_tostring(L, i));
+                break;
+            }
+            case LUA_TBOOLEAN: {
+                printf(lua_toboolean(L, i) ? "true" : "false");
+                break;
+            }
+            case LUA_TNUMBER: {
+                printf("%g", lua_tonumber(L, i));
+                break;
+            }
+            default: {
+                printf("%s", lua_typename(L, t));
+                break;
+            }
+        }
+        printf("    ");
+    }
+    printf("\n");
+}
