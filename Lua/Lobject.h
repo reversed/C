9e0b46b4d3eb5e6b03216f334f98bef01d017882
reversed <leichen.usst@gmail.com>
2016-11-15 16:51:26 +0800

    add Lobject.h

---
+++ Lua/Lobject.h
@@ -0,0 +1,10 @@
+/*
+** Common type for all collectable objects
+*/
+typedef struct GCObject GCObject;
+
+/*
+** Common Header for all collectable objects (in macro form, to be
+** included in other objects)
+*/
+#define CommonHeader    GCObject *next; lu_byte tt; lu_byte marked
